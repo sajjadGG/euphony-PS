189a4ae2f11aac5eedd578baa4fc8c068e0c97a4
Sajad Ramezani <sajad47@bc11447.int.ets1.calculquebec.ca>
2023-04-12 17:12:59 -0400

    add parallel

--- run_phog_learner.py
+++ run_phog_learner.py
@@ -3,19 +3,38 @@ from pickle import load
 import logging
 import time
 import os
+import argparse
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_clusters',type=int)
+# parser.add_argument('--config_id',type=str)
+args = parser.parse_args()
+args_dict = vars(args)
+file_extend = "_".join(["{}-{}".format(k,args_dict[k]) for k in args_dict])
+LOG_FILE_NAME = f'result_time_{file_extend}.log'
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S',
-    filename='result_time.log',
+    filename=LOG_FILE_NAME,
     filemode='a',
 )
+
 
 TRAIN_PATH = 'benchmarks/string/train/'
 TEST_PATH = 'benchmarks/string/test/'
 
-with open('../processed_data.pickle','rb') as f:
+
+command = f'cd ..;python run_data_processing.py --n_clusters {args.n_clusters}'
+logging.info('START CLUSTERING')
+result = subprocess.run(command,capture_output=True, shell=True)
+logging.info('FINSISH CLUSTERING')
+logging.info(result.stdout.decode())
+logging.info(result.stderr.decode())
+with open(f'../processed_data_cluster_{args.n_clusters}.pickle','rb') as f:
     d = load(f)
 
 d_rev = {} #cluster_id : all files
@@ -43,32 +62,34 @@ for k in d_rev:
 #     db = load(f)
 
 ### Training
-# phog_learner_command = "bin/run_phog_learner phog_str_{} {}"
-# for k in d_rev:
-#     files = " ".join(list(map(lambda x:f'{TRAIN_PATH}{x}.sl' ,db['{}-train'.format(k)])))
-#     command = phog_learner_command.format(k,files)
-#     print(command)
-#     ret = subprocess.run(command, capture_output=True, shell=True)
-#     print(ret.stdout.decode())
-#     print(f'cluster {k} is done')
-# print('TRAINING DONE')
+phog_learner_command = "bin/run_phog_learner phog_str_{} {}"
+for k in d_rev:
+    files = " ".join(list(map(lambda x:f'{TRAIN_PATH}{x}.sl' ,db['{}-train'.format(k)])))
+    command = phog_learner_command.format(f"{k}_n_clusters_{args.n_clusters}",files)
+    # print(command)
+    ret = subprocess.run(command, capture_output=True, shell=True)
+    # print(ret.stdout.decode())
+    number_of_training_data = len(db['{}-train'.format(k)])
+    logging.info(f'cluster {k} is done with {number_of_training_data} training data')
+logging.info('TRAINING DONE')
+
 ## Testing
 from timeit import timeit
 from tqdm import tqdm
 cluster_test_time = {}
 cluster_output = {}
-phog_guide_command = "time timeout 3m bin/run_with_new_phog {} {}" 
+phog_guide_command = "time timeout 3m bin/run_with_new_phog {} {} {}" 
 
-small_problems = ['exceljet1','exceljet4','exceljet3','stackoverflow4','stackoverflow1','stackoverflow3'
-                  ,'stackoverflow8','stackoverflow5','stackoverflow9','phone-5','phone-5-long',
-                  'phone-5-long-repeat','phone-5-short','phone-6','phone-6-long',
-                  'phone-6-long-repeat','phone-6-short','phone-7','phone-7-long',
-                  'phone-7-long-repeat','phone-7-short']
-# small_problems = ['stackoverflow1']
+# small_problems = ['exceljet1','exceljet4','exceljet3','stackoverflow4','stackoverflow1','stackoverflow3'
+#                   ,'stackoverflow8','stackoverflow5','stackoverflow9','phone-5','phone-5-long',
+#                   'phone-5-long-repeat','phone-5-short','phone-6','phone-6-long',
+#                   'phone-6-long-repeat','phone-6-short','phone-7','phone-7-long',
+#                   'phone-7-long-repeat','phone-7-short']
+small_problems = ['stackoverflow8','stackoverflow9','phone-5-short']
 cluster_threshold = 10
 logging.info('----------- Testing Start ----------------')
 for k in d_rev:
-    grammar_cluster = f"phog_str_{k}"
+    grammar_cluster = f"{k}_n_clusters_{args.n_clusters}"
     grammar_all = "phog_str3all_train"
 
     test_files = list(map(lambda x:f'{TEST_PATH}{x}.sl' ,db['{}-test'.format(k)]))
@@ -82,12 +103,12 @@ for k in d_rev:
         for t in tqdm(test_files):
             if t.split('/')[-1][:-3] not in small_problems:
                 continue
-            command = phog_guide_command.format(grammar_cluster,t)
-            # print(f"SOlVING {t} WITH COMMAND {command}")
+            command = phog_guide_command.format(grammar_cluster,LOG_FILE_NAME,t)
+            # logging.info(f"SOlVING {t} WITH COMMAND {command}")
             
             cluster_time = subprocess.run(command, capture_output=True, shell=True)
             # cluster_time = timeit(stmt = stmts, setup = "import subprocess", number = 1)
-            command = phog_guide_command.format(grammar_all,t)
+            command = phog_guide_command.format(grammar_all,LOG_FILE_NAME,t)
             all_phog_time = subprocess.run(command, capture_output=True, shell=True) #euphony
             # all_phog_time = timeit(stmt = stmts, setup = "import subprocess", number = 1)
             cluster_test_time[k].append((t,cluster_time.stderr.decode(),all_phog_time.stderr.decode()))
@@ -98,11 +119,11 @@ for k in cluster_test_time:
     if cluster_test_time[k]!=[]:
         for p in cluster_test_time[k]:
             all_time.append(p[1])
-            print(p[0])
-            print(p[1])
-            print(p[2])
-            print('------------')
+            logging.info(p[0])
+            logging.info(p[1])
+            logging.info(p[2])
+            logging.info('------------')
 
-print(f'average_time : {sum(all_time)/len(all_time)}')
+logging.info(f'average_time : {sum(all_time)/len(all_time)}')
 
 # # file -> hyperparameters -> result
